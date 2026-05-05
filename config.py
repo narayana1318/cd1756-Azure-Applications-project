0bbadd308db582b15c1dea370e413e28127c3746
Narayana <dogiparthy.veeravenkatanarayana@chevron.com>
2026-05-06 00:25:23 +0530

    updated config writeup!

--- config.py
+++ config.py
@@ -32,8 +32,7 @@ class Config(object):
 
     CLIENT_ID = os.getenv("CLIENT_ID")
 
-    REDIRECT_PATH = "https://udacitycms-hsggc0fbb9cpfyav.canadacentral-01.azurewebsites.net/auth"
-    # "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
     # You can find the proper permission names from this document
     # https://docs.microsoft.com/en-us/graph/permissions-reference
